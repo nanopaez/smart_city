92008500881aa2a6245cc26be33b730c0dcbb98f
Hernán Páez <hernanpaezcampos@gmail.com>
2023-10-17 07:38:14 -0300

    cambio color al navbar

--- lib/widgets/napbar.dart
+++ lib/widgets/napbar.dart
@@ -18,11 +18,14 @@ class CustomNavBarState extends State<NavBar> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      backgroundColor: const Color(0xFF77cb17), // Color de fondo
       currentIndex: widget.currentIndex,
       onTap: widget.onTap,
+      selectedItemColor: Colors.white, // Color de ícono seleccionado
+      unselectedItemColor: const Color.fromARGB(
+          255, 11, 83, 27), // Color de ícono no seleccionado
       items: const [
         BottomNavigationBarItem(
-          backgroundColor: Color.fromARGB(255, 251, 118, 118),
           icon: Icon(Icons.home),
           label: 'Inicio',
         ),
